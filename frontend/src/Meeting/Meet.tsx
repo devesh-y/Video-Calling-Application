6ed0537524bf99d8fdf25d845f6af83ca65fe4db
Devesh Yadav <ydevesh136@gmail.com>
2023-05-31 23:56:23 +0530

    rtc offer

--- frontend/src/Meeting/Meet.tsx
+++ frontend/src/Meeting/Meet.tsx
@@ -1,4 +1,4 @@
-import { useContext, useState, useEffect,useCallback } from "react";
+import { useContext, useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { SocketContext } from "../Socket/SocketClient";
 import "./meet.css"
--- frontend/src/WebRTC/p2p.ts
+++ frontend/src/WebRTC/p2p.ts
@@ -1,10 +1,17 @@
 class peerservice {
 
     peer: RTCPeerConnection;
-    constructor(peer) {
+    constructor() {
         this.peer = new RTCPeerConnection({ 
             'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] 
         })
+    }
+    async getoffer(){
+        if(this.peer){
+            const offer= await this.peer.createOffer();
+            await this.peer.setLocalDescription(new RTCSessionDescription(offer));
+
+        }
     }
 
 }
